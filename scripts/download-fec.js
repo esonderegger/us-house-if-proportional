79d933520fa41446f1ce2dd46fc8ee05d2aa296a
Evan Sonderegger <evan@rpy.xyz>
2017-01-13 17:03:24 -0500

    create directory for xls if it does not exist

--- scripts/download-fec.js
+++ scripts/download-fec.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const path = require('path');
 
 const years = [
   2004,
@@ -10,12 +11,29 @@ const years = [
   2014,
 ];
 
+function directoryExists(path) {
+  try {
+    return fs.statSync(path).isDirectory();
+  } catch (err) {
+    return false;
+  }
+}
+
+const xlsDir = path.join(process.cwd(), 'xls');
+
+if (!directoryExists(xlsDir)) {
+  fs.mkdirSync(xlsDir);
+}
+
 years.forEach((year) => {
   const url = 'http://www.fec.gov/pubrec/fe' + year + '/federalelections' +
     year + '.xls';
   const xlsPath = 'xls/' + year + '.xls';
   let xlsFile = fs.createWriteStream(xlsPath);
-  const request = http.get(url, (response) => {
+  const request = http.get(
+    url,
+    // {headers: {accept: 'application/vnd.ms-excel'}},
+    (response) => {
       response.pipe(xlsFile);
       response.on('end', () => {
         console.log('successfully downloaded: ' + url);
